2ba5078f7bda95672e384c212433d0d1591d760b
Saxley <64879489+Saxley@users.noreply.github.com>
2020-09-01 14:57:21 -0300

    se corrigieron los src

--- js/traductorBinario.js
+++ js/traductorBinario.js
@@ -15,7 +15,7 @@ aplicar.addEventListener("click", traducir);
 var alfabeto = [" ","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var num = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
 //Tema
-var src=["assets/noche.png","assets/dia.png"];
+var src=["assets/Noche.png","assets/Dia.png"];
 var color=["#000","#fff"];
 
 //FUNCIONES
