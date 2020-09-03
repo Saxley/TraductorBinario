c1ed15d6f05c8c17ac83a3145c4c8f7bccd89651
Luigi <@Saxley>
2020-09-02 21:56:09 -0300

    agregar documentacion

--- js/traductorBinario.js
+++ js/traductorBinario.js
@@ -1,21 +1,38 @@
 //VARIABLES
-var  into, txt_box, aplicar, traduccion, DN, imgT;
+var  into, txt_box, aplicar, traduccion, DN, imgT,inverso,si;
 DN = false;
+si = true;
 //Busqueda x id
 txt_box = document.getElementById("txt_box");
 aplicar = document.getElementById("aplicar");
 traduccion = document.getElementById("traduccion");
 imgT = document.getElementById("tema");
+inverso=document.getElementById("cambio");
 //Listeners
-aplicar.addEventListener("click", traducir);
-
+aplicar.addEventListener("click", election);
+inverso.addEventListener("click", invertir);
 //DICCIONARIO
 //Tema
 var link_src = ["assets/Noche.png", "assets/Dia.png"];
 var color = ["#000", "#fff"];
 
 //FUNCIONES
-
+function invertir(){
+  if(si){
+    si=false;
+    txt_box.placeholder="Binario a ABC";
+  }else{
+    si=true;
+    txt_box.placeholder="ABC a Binario";
+  }
+}
+function election(){
+  if(si){
+    traducir();
+  }else if(!si){
+    binario();
+  }
+}
 //BOTON TRADUCIR
 function traducir() {
   //ENTRADA Y CAPTURA DE DATOS
@@ -33,9 +50,24 @@ function traducir() {
     text_final = text_final+" "+into[i];
   }
   traduccion.innerHTML = text_final;
+  txt_box.value="";
 }
-
-
+function binario(){
+ //ENTRADA Y CAPTURA DE DATOS
+  var text_final = "";
+  into = txt_box.value;
+  //transforma el valor a un array (lo que tomara como referencia, tamañano del array)
+  into = into.split(" ");
+  
+  for(var i in into){
+    into[i]=parseInt(into[i],2);
+    into[i]=into[i].toString(10);
+    into[i]=String.fromCharCode(into[i]);
+    text_final = text_final+into[i];
+  }
+ traduccion.innerHTML = text_final;
+ txt_box.value="";
+}
 //BOTON TEMA
 function tema() {
   if (DN) {
